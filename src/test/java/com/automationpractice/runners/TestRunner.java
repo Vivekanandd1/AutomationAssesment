262cf3ff4c58f745fc642a6474d7981afc52aa13
vivkekanandd1 <vivekanandd1@live.com>
2025-07-18 18:23:12 +0530

    Removed Junit runner code and added TestNG

--- src/test/java/com/automationpractice/runners/TestRunner.java
+++ src/test/java/com/automationpractice/runners/TestRunner.java
@@ -1,15 +1,12 @@
 package com.automationpractice.runners;
 
-import org.junit.runner.RunWith;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
-@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "classpath:Features",
 		glue = "com.automationpractice.STEPDEFS",
-		tags =" ",
+		tags ="",
 		plugin= {
 				"pretty",
 				"html:target/Reports/htmlrepo.html",
@@ -21,6 +18,8 @@ import io.cucumber.junit.CucumberOptions;
 		
 		)
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
+	
+
 
 }
